a3ac120368bb05e71c1a77e637ffbdbe5e76a2da
Harning Laptop <hejthenerd@gmail.com>
2025-04-01 10:17:07 +0200

    Initial commit

---
+++ src/main/java/org/example/projekt4_gruppe_3/Projekt4Gruppe3Application.java
@@ -0,0 +1,13 @@
+package org.example.projekt4_gruppe_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt4Gruppe3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt4Gruppe3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projekt4_gruppe_3/Projekt4Gruppe3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projekt4_gruppe_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt4Gruppe3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
