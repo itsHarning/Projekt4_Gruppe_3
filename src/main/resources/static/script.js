35f2b1a34f118fec54a938aff0da73c04b1c1c13
Harning Laptop <hejthenerd@gmail.com>
2025-04-06 21:21:27 +0200

    popup login/signup base setup
    not linked up to database/spring, so can't actually be used yet

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,21 @@
+const showPopupBtn = document.querySelector(".login-btn")
+const formPopup = document.querySelector(".form-container")
+const hidePopupBtn = document.querySelector(".form-container .close-btn")
+const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a")
+
+// show form popup
+showPopupBtn.addEventListener("click", () => {
+    document.body.classList.toggle("show-popup")
+});
+
+// hide form popup
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+
+loginSignupLink.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        // if the clicked link is signup, then add "show-signup" class to the form popup, else remove the class
+        formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup")
+    })
+})
